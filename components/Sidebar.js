a09bb74e11d6be7d3aed6ab3ee36ac71046e7e8c
Norbert Kwarciak <norbert.kwarciak@gmail.com>
2021-04-26 22:36:36 +0200

    Chat list

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,3 +1,5 @@
+import { useAuthState }    from 'react-firebase-hooks/auth';
+import { useCollection }   from 'react-firebase-hooks/firestore';
 import styled              from 'styled-components';
 import { Avatar,
          IconButton,
@@ -6,24 +8,36 @@ import ChatIcon            from '@material-ui/icons/Chat';
 import MoreVertIcon        from '@material-ui/icons/MoreVert';
 import SearchIcon          from '@material-ui/icons/Search';
 import * as EmailValidator from 'email-validator';
+import Chat                from './Chat';
+import { auth, db  }       from '../firebase';
 
 
 export default function Sidebar() {
+  const [user] = useAuthState(auth);
+  const userChatRef = db.collection('chats').where('users', 'array-contains', user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
 
   const createChat = () => {
     const input = prompt('Please enter an email address for the user you wish to chat with');
 
     if (!input) return null;
 
-    if (EmailValidator.validate(input)) {
+    if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email) {
       // we need to add the chat into the DB 'chats' collection
+      db.collection('chats').add({
+        users: [user.email, input]
+      })
     }
   };
+
+  const chatAlreadyExists = recipientEmail => {
+    !!chatsSnapshot?.docs.find(chat => chat.data().users.find(user => user === recipientEmail)?.length > 0);
+  }
 
   return (
     <Container>
       <Header>
-        <UserAvatar />
+        <UserAvatar onClick={() => auth.signOut()} />
 
         <IconsContainer>
           <IconButton>
@@ -41,6 +55,14 @@ export default function Sidebar() {
       </Search>
 
       <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+
+      {chatsSnapshot?.docs.map(chat => (
+        <Chat
+          key={chat.id}
+          id={chat.id}
+          user={chat.data().users}
+        />
+      ))}
     </Container>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import { useEffect }    from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import firebase         from 'firebase';
 import Login            from './login';
 import Loading          from '../components/Loading';
 import { auth, db }     from '../firebase';
@@ -7,6 +9,16 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set({
+        email: user.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        photoUrl: user.photoURL
+      }, { merge: true })
+    }
+  }, [user]);
 
   if (loading) return <Loading />;
   if (!user) return <Login />;
